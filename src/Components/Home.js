b5857cc29c3302abf7badbdce5720655ff312412
arerimr <raydelys.morrobelr@gmail.com>
2023-04-17 21:40:21 -0400

    added css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div>
+        <div className="home">
             <h1>
                 Welcome to the best budgeting app
             </h1>
--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -2,7 +2,7 @@
 
 export default function NotFound() {
     return (
-        <div>
+        <div className="NotFound">
             <h1>404 Page Not Found 404</h1>
         </div>
     )
