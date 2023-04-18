6c3605d8b8bd0e20b17bfd82a5080d13f43d4471
arerimr <raydelys.morrobelr@gmail.com>
2023-04-18 19:22:44 -0400

    display transactions

--- src/Components/transactions.js
+++ src/Components/transactions.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
-// const API = process.env.REACT_APP_API_URL
-const API = 'http://localhost:8889'
+const API = process.env.REACT_APP_API_URL
+// const API = 'http://localhost:8889'
 console.log(API)
 
 export default function Transactions() {
@@ -20,13 +20,27 @@ export default function Transactions() {
         <div>
             <h1>Transactions</h1>
             <table>
-                <tr>
-                    <th>DATE</th>
-                    <th>ITEM NAME</th>
-                    <th>AMOUNT</th>
-                    <th>PRIORITY</th>
-                    <th>DONE</th>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th>DATE</th>
+                        <th>ITEM NAME</th>
+                        <th>AMOUNT</th>
+                        <th>PRIORITY</th>
+                        <th>DONE</th>
+                    </tr>
+
+                    {
+                        transactions.map((trans) => {
+                            return <tr>
+                                <td>{trans.date}</td>
+                                <td>{trans.item_name}</td>
+                                <td>${trans.amount}</td>
+                                <td>{trans.priority === "high" ? trans.priority === "medium" ? "📌" : "📌📌📌" : "📌📌"}</td>
+                                <td>{trans.completed ? "✔" : "𝙓"}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
             </table>
         </div>
     )
