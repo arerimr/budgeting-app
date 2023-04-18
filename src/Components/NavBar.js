09686d2ff497279c536ddc0924b030924ac23ed4
arerimr <raydelys.morrobelr@gmail.com>
2023-04-17 21:26:08 -0400

    added links to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 export default function NavBar() {
     return (
         <div className="NavBar">
-            <h1>Budget App by M R</h1>
-            <button>NEW TRANSACTION</button>
+            <Link to='/transactions'><h1>Budget App by M R</h1></Link>
+            <button><Link to='/transactions/new'>NEW TRANSACTION</Link></button>
         </div>
     )
 }
