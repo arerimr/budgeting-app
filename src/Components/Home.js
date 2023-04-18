b77d4222fb0da0072f78c96a72466aa9e9266012
arerimr <raydelys.morrobelr@gmail.com>
2023-04-17 21:29:11 -0400

    finished home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
@@ -6,6 +6,7 @@ export default function Home() {
             <h1>
                 Welcome to the best budgeting app
             </h1>
+            Click <Link to='/transactions'>HERE</Link> to view all transactions
         </div>
     )
 }
