6e653ef8be8ea816da0267d5428bc6c3b1a3de1c
arerimr <raydelys.morrobelr@gmail.com>
2023-04-18 20:35:22 -0400

    setup transaction

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,6 +1,23 @@
-
+import axios from "axios"
+import { useParams, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+const API = process.env.REACT_APP_API_URL
 
 export default function Transaction() {
+    const [transaction, setTransaction] = useState({})
+
+    const { index } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios
+            .get(`${API}/transactions/${index}`)
+            .then((res) => {
+                setTransaction(res.data)
+            }).catch((e) => console.log(e))
+    }, [])
+
     return (
         <div>
             <h1>Transaction Details</h1>
